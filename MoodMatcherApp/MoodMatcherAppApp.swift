f208d5d846e9b4405ee84a47945284cf84809e65
kwk Cecilia .C <cc.comar36@gmail.com>
2025-07-31 09:35:33 -0400

    Initial Commit

---
+++ MoodMatcherApp/MoodMatcherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMatcherAppApp.swift
+//  MoodMatcherApp
+//
+//  Created by Scholar on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMatcherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
